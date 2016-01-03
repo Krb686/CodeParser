d3c7ff6df9824e40c21f8678f8c79a7c3ebd6b38
Kevin Briggs <krb686@gmail.com>
2016-01-03 12:36:31 -0500

    Update run_driver.sh

--- run_driver.sh
+++ run_driver.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 PROJECT_PATH="/home/kevin/gitrepos/CodeParser"
-export LD_LIBRARY_PATH="$PROJECT_PATH/bin/lib/"
-objdump -StTwx "$PROJECT_PATH/bin/test/test_binary">"$PROJECT_PATH/bin/test/test_dump.disasm"
+TEST_BINARY="$PROJECT_PATH/bin/test/test_binary"
+TEST_DISASM="$PROJECT_PATH/bin/test/test_dump.disasm"
+LD_PATH_ADD="$PROJECT_PATH/bin/lib/"
+
+if [[ ! "$LD_LIBRARY_PATH" == *"$LD_PATH_ADD"* ]]; then
+	printf "Exporting lib folder...\n"
+	export LD_LIBRARY_PATH="$LD_PATH_ADD"
+fi
+
+if [[ ! -e "$TEST_DISASM" ]]; then
+	if [[ ! -e "$TEST_BINARY" ]]; then
+		printf "Test binary and objdump do not exist! Please clean and build the project first!\n"
+		exit 1
+	fi
+	printf "Object dumping $TEST_BINARY to $TEST_DISASM\n"
+	objdump -StTwx "$TEST_BINARY">"$TEST_DISASM"
+fi
+
 "$PROJECT_PATH/bin/driver/CodeParser_driver"
